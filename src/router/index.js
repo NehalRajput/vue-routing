96026e1065fa101df48119271ae1f1ab61fead57
NehalRajput <nehal17.ast@gmail.com>
2025-04-11 15:44:06 +0530

    route meta fields

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,74 @@
-
-
-import { createRouter , createWebHistory} from 'vue-router'
+import { createRouter , createWebHistory } from 'vue-router'
 import sourceData from '@/data.json'
-const routes = [
-  
-  {
-    path: '/destination/:id/:slug',
-    name: 'destination.show',
-    component: () => import('@/views/DestinationShow.vue'),
-    props: route => ({ ...route.params, id: parseInt(route.params.id) }),
-    beforeEnter(to, from){
-      const exists = sourceData.destinations.find(
-        destination => destination.id === parseInt(to.params.id)
-      )
-      if(!exists) return {
-        name: 'NotFound',
-     
-        params: { pathMatch: to.path.split('/').slice(1) },
-        query: to.query,
-        hash: to.hash,
-      }
-    },
-    children:[
-      {
-        path: ':experienceSlug',
-        name: 'experience.show',
-        component: () => import('@/views/ExperienceShow.vue'),
-        props: route=> ({...route.params, id: parseInt(route.params.id)})
-      }
-    ]
-  },
 
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue')
   },
   {
+    path: '/protected',
+    name: 'protected',
+    component: () => import('@/views/Protected.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/destination/:id/:slug',
+    name: 'destination.show',
+    component: () => import('@/views/DestinationShow.vue'),
+    props: route => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to, from) {
+      const exists = sourceData.destinations.find(
+        destination => destination.id === parseInt(to.params.id)
+      )
+      if (!exists) {
+        return {
+          name: 'NotFound',
+          params: { pathMatch: to.path.split('/').slice(1) },
+          query: to.query,
+          hash: to.hash
+        }
+      }
+    },
+    children: [
+      {
+        path: ':experienceSlug',
+        name: 'experience.show',
+        component: () => import('@/views/ExperienceShow.vue'),
+        props: route => ({ ...route.params, id: parseInt(route.params.id) })
+      }
+    ]
+  },
+  {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: ()=> import('@/views/NotFound.vue')
+    component: () => import('@/views/NotFound.vue')
   }
 ]
-
-
-
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  //linkActiveClass:'vue-school-active-link',
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || new Promise(resolve => {
+      setTimeout(() => resolve({ top: 0 }), 300)
+    })
+  }
+})
+
+
+router.beforeEach((to, from) => {
+  if (to.meta.requiresAuth && !window.user) {
+    return { name: 'login' }
+  }
 })
 
 export default router
