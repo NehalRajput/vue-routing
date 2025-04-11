ee5972de04c178198cb9d20280611c5af798c467
NehalRajput <nehal17.ast@gmail.com>
2025-04-11 15:19:09 +0530

    page not  found

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: ()=> import('@/views/NotFound.vue')
   }
 ]
 
